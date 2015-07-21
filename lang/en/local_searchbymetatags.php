664681db7b9c430776866e2b19102f73520cfb05
DevonKS <devonsteenberg@hotmail.com>
2015-07-21 21:22:24 +1200

    Corrected Plugin name

--- lang/en/local_searchbymetatags.php
+++ lang/en/local_searchbymetatags.php
@@ -13,4 +13,4 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-$string['pluginname'] = 'Question bank search by tags';
+$string['pluginname'] = 'Question bank search by  meta tags';
--- version.php
+++ version.php
@@ -18,5 +18,5 @@ $plugin->version   = 2014012900;
 $plugin->release   = '2.2.7';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->requires  = 2011120500;
-$plugin->component = 'local_searchbytags';
+$plugin->component = 'local_searchbymetatags';
 
